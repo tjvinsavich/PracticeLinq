48f475d6aa7d55ac3dce66c333c5fe569f81ad36
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-29 11:59:52 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> games = new List<string>() { "Monkey Island", "Day of the Tentacle", "Sam and Max" };
+
+            var sortedGames = games.OrderBy(game => game.Length);
+
+            foreach (var game in sortedGames)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
